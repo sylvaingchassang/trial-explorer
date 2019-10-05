a3cb313ed91255ec283db15295669e3bad7fb570
sylvain <sylvain.chassang@gmail.com>
2019-10-05 17:47:17 -0400

    config: more robust PRIVATE_CONFIG_LOC, requirements update
    apt-requirements

--- pdaactconn/config.py
+++ pdaactconn/config.py
@@ -1,10 +1,12 @@
 """
 Connection configurations
 """
+from os import path
 
-PRIVATE_CONFIG_LOC = r'./config/private/credentials.private.ini'  # default path to the db credentials config files
+PRIVATE_CONFIG_LOC = path.join(path.dirname(__file__), '..', 'config',
+                               'private', 'credentials.private.ini')
 
-AACT_HOST = 'aact-db.ctti-clinicaltrials.org'  # url of the hosted AACT database
+AACT_HOST = 'aact-db.ctti-clinicaltrials.org'
 AACT_DB = 'aact'
 
 LOCAL_HOST = 'localhost'
